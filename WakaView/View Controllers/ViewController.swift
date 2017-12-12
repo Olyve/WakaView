d2730239a868d786d8c2c0fb98b3078dd4436fa0
Sam Galizia <samgalizia@gmail.com>
2017-12-12 12:53:08 -0800

    Main View now fetches user Stats on load

--- WakaView/View Controllers/ViewController.swift
+++ WakaView/View Controllers/ViewController.swift
@@ -6,18 +6,37 @@
 //  Copyright © 2017 Sam Galizia. All rights reserved.
 //
 
+import Moya
+import SwiftyJSON
 import UIKit
 
 class ViewController: UIViewController {
-
+  private let provider = MoyaProvider<WakaTimeAPIService>()
+  @IBOutlet weak var testOutput: UITextView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    getStats()
   }
 
-  @IBAction func logoutUser() {
-    UserDefaults.standard.removeObject(forKey: "token")
-    UserDefaults.standard.removeObject(forKey: "refresh_token")
-  }
 }
 
+// MARK: - Helper methods
+private extension ViewController {
+  // TODO: You could easily make this return a JSON value and then
+  // take the JSON and put the data into charts from here.
+  func getStats() {
+    provider.request(.stats(range: .last_7_days)) { result in
+      switch result {
+      case let .success(moyaResponse):
+        let data = moyaResponse.data
+        let json = JSON(data)
+        print(json)
+        self.testOutput.text = json.rawString()
+      case let .failure(error):
+        print(error.localizedDescription)
+      }
+    }
+  }
+}
